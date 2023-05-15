2e5bf18be2f6d8126aafec3aa681de783b3c37e9
bstanwell-acs <benjaminstanwell@gmail.com>
2023-05-15 12:50:10 +1000

    bstanwell upload

---
+++ bBoard/src/test2.ino
@@ -0,0 +1,193 @@
+/*
+ * Project test2
+ * Description:
+ * Author:
+ * Date:
+ */
+
+#include <Wire.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+
+
+
+#define LDR_PIN 16 // A3
+#define SOUND_PIN 19 // A0
+#define TEMP_PIN 17 // A2
+#define PIR_PIN 5 // D5
+
+#define PIXEL_COUNT 2
+#define PIXEL_PIN D2
+#define PIXEL_TYPE WS2812B
+
+#define MIC_BUFFER_SIZE 512
+
+Adafruit_SSD1306 display(-1);
+
+
+
+int ldrValue, soundValue, temperatureValue;
+volatile uint64_t uinMovementTime = 0;
+uint64_t uinMicTime = 0;
+uint32_t aruinMicBuffer[MIC_BUFFER_SIZE];
+
+void fnvdMovementIsr();
+bool fnboCheckMovement();
+float fnflGetMicRMS();
+float fnflRMStodBa(float flRmsInput);
+void fnvdReadADCMic();
+float fnflGetTemperaturemV();
+float fnflGetTemperatureC(float flTempmV);
+// float fnflGetTempOneWire();
+
+void setup(){
+  
+  attachInterrupt(PIR_PIN, fnvdMovementIsr, CHANGE);
+  pinMode(PIR_PIN, INPUT);
+
+  // sensor.begin();
+  // sensor.setResolution(11);
+
+  Wire.begin();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  display.display(); // show splashscreen
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("Hello World");
+  display.display();
+  display.clearDisplay();   // clears the screen and buffer
+
+
+  for(uint32_t i = 0; i < MIC_BUFFER_SIZE; i++){aruinMicBuffer[i] = 2048;}
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // fnvdReadADCMic();
+  float flmVRms = fnflGetMicRMS(), flTempmV = fnflGetTemperaturemV();
+  for(int i = 0; i < 9; i++){flTempmV += fnflGetTemperaturemV();}
+  flTempmV /= 10;
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("MIC:");
+  // display.print(flmVRms);
+  // display.println(" mV");
+  display.print(fnflRMStodBa(flmVRms));
+  display.println(" dBA");
+  display.println("Temperture");
+  // display.print(flTempmV);
+  // display.println(" mV");
+  display.print(fnflGetTemperatureC(flTempmV));
+  display.println(" C");
+  // display.println("1-Wire Temp");
+  // display.print(fnflGetTempOneWire());
+  // display.println(" C");
+  if(fnboCheckMovement()){display.println("MOVEMENT DETECTED!");}
+  display.display();
+  
+  
+}
+
+void fnvdMovementIsr(){
+  uinMovementTime = millis();
+//   display.clearDisplay();
+//   display.setCursor(0, 0);
+//   display.println("MOVEMENT DETECTED!");
+//   display.display();
+}
+
+bool fnboCheckMovement(){
+  return (millis() - uinMovementTime < 1000);
+}
+
+
+float fnflGetMicRMS(){
+  float flRms = 0;
+  uint64_t uinMean = 0;
+  
+
+
+  for(uint32_t i = 0; i < MIC_BUFFER_SIZE; i++){
+    aruinMicBuffer[i] = analogRead(SOUND_PIN);
+    uinMean += aruinMicBuffer[i];
+  }
+  uinMean /= MIC_BUFFER_SIZE;
+
+  for(uint32_t i = 0; i < MIC_BUFFER_SIZE; i++){
+    flRms += (aruinMicBuffer[i] - uinMean) * (aruinMicBuffer[i] - uinMean);
+  }
+  flRms /= MIC_BUFFER_SIZE;
+  flRms = sqrt(flRms);
+
+  flRms = (flRms - 2.4595) / 1.2254;
+  return flRms;
+}
+
+void fnvdReadADCMic(){
+  static uint32_t stuinCounter = 0;
+  if(stuinCounter >= MIC_BUFFER_SIZE){stuinCounter = 0;}
+  aruinMicBuffer[stuinCounter] = analogRead(SOUND_PIN);
+  stuinCounter++;
+}
+
+float fnflRMStodBa(float flRmsInput){
+  return 10.1 * logf(flRmsInput) + 26.223;
+}
+
+float fnflGetTemperaturemV(){
+  return (analogRead(TEMP_PIN) * 3300 / 4096);
+}
+
+
+float fnflGetTemperatureC(float flTempmV){
+  return flTempmV * 0.0445 + 13.137;
+  // return ((flTempmV) * 0.1) - 1.24;
+}
+
+// float fnflGetTempOneWire(){
+//   static float tmp = 0;
+//   if (sensor.isConversionComplete())
+//   {
+//   // Serial.print("Temp: ");
+//   float tmp= sensor.getCRCTempC();
+//   sensor.requestTemperatures();
+//   }
+//   return tmp;
+// }
+
+
+/*
+  // The core of your code will likely live here.
+  float sum = 0, level = 0;
+  for ( int i = 0 ; i < 1024; i++){  
+    sum += analogRead(SOUND_PIN) - 2048;
+  } 
+  level = sum / 1024; // Calculate the average value 
+  // ldrValue = analogRead(LDR_PIN);
+  soundValue = analogRead(SOUND_PIN);
+  temperatureValue = analogRead(TEMP_PIN);
+  // Log.info("analogvalue=%d", ldrValue);
+  
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  // display.print("LDR value: ");
+  // display.println(ldrValue);
+  display.print("MIC value: ");
+  display.println(soundValue);
+  display.print("DB value: ");
+  display.println(level);
+  display.print("TMP value: ");
+  display.println(temperatureValue);
+  // display.print("D5 Val: ");
+  // display.println(digitalRead(PIR_PIN));
+  // display.print("MOV: ");
+  // display.println(fninCheckMovement());
+  if(fnboCheckMovement())
+    display.println("MOVEMENT DETECTED!");
+  display.display();
+
+*/
---
+++ bBoard/target/5.3.0/argon/obj/src/user_module.o.d
@@ -0,0 +1,6 @@
+c\:/Users/benja/Documents/year5/sem1/elec4740/particle/test2/target/5.3.0/argon//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
