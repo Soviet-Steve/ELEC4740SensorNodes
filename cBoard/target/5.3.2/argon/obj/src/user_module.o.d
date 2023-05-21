ba3f4925dbff2e4f6b9e6312857f5265ac51efc8
Claegtun <clayton.john.carlon@gmail.com>
2023-05-22 09:58:00 +1000

    Moved the code of Node-2 from node_2 to cBoard; compiled the code for Node-2.

---
+++ cBoard/target/5.3.2/argon/obj/src/user_module.o.d
@@ -0,0 +1,6 @@
+c\:/Users/Clayton/Documents/University/Fifta_Gear/Forma_Siexmonaþ/ELEC4740/Project/Particle/ELEC4740SensorNodes/cBoard/target/5.3.2/argon//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
